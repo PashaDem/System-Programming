3fac4a46d99aec06b092f99d26be52e362cffc8f
PashaDem <dem-pasha@lenta.ru>
2023-10-21 02:44:22 +0300

    feat: add secondary projects for testing

---
+++ test_app1/test_app1/test_app1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    string appName = "test_app1";
+
+    for (int counter = 0; counter < 100; ++counter)
+    {
+        sum += counter;
+        cout << appName << ":: Iteration number ::" << counter << endl;
+        Sleep(1000);
+        if (counter == 10) {
+            throw std::invalid_argument("each time counter == 10 I throw an exception");
+        }
+    }
+    cout << sum << endl;
+}
